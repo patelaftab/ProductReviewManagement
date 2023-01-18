4a7d276fd424dd01aedd278343c0fe78f004d1f4
patelaftab <10patelaftab@gmail.com>
2023-01-18 11:50:34 +0530

    [Aftab] Created a Product Review Class with Given Field Values

---
+++ Day-35_Linque/Program.cs
@@ -0,0 +1,29 @@
+﻿using Day_35_Linque;
+
+namespace ProductReviewManagement
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Problem");
+            List<ProductReview> productReviews = new List<ProductReview>()
+            {
+                new ProductReview() { ProductId=1,userId=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview() { ProductId = 1,userId=1,Rating=5,Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 2, userId = 1, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 2, userId = 1, Rating = 2, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 2, userId = 1, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 3, userId = 1, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 4, userId = 1, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 4, userId = 1, Rating = 3, Review = "Average", isLike = true },
+                new ProductReview() { ProductId = 5, userId = 1, Rating = 1, Review = "Bad", isLike = false },
+                new ProductReview() { ProductId = 5, userId = 1, Rating = 5, Review = "Bad", isLike = false }
+            };
+            foreach(var list in productReviews)
+            {
+                Console.WriteLine("Product Id:"+list.ProductId+  "UserId:"+list.userId + "Rating:"+ list.Rating +"Review: "+ list.Rating + "isLike: " + list.isLike);
+            }
+        }
+    }
+}
